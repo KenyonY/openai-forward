dec5cdc65e399e5a3aabdd16d90f40c1e4205d5f
kunyuan <kunyuany@163.com>
2023-05-02 19:06:29 +0800

    update pytest

---
+++ tests/utils.py
@@ -0,0 +1,20 @@
+from sparrow import ls
+import os
+import shutil
+
+def rm(*file_pattern: str, rel=False):
+    """Remove files or directories.
+    Example:
+    --------
+        >>> rm("*.jpg", "*.png")
+        >>> rm("*.jpg", "*.png", rel=True)
+    """
+    path_list = ls(".", *file_pattern, relp=rel, concat="extend")
+    for file in path_list:
+        if os.path.isfile(file):
+            print("remove ", file)
+            os.remove(file)
+            # os.system("rm -f " + file)
+        elif os.path.isdir(file):
+            shutil.rmtree(file, ignore_errors=True)
+            print("rm tree ", file)
