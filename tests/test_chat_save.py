b9e9b3af0698cea295816cf6a7c18c095eb02d4d
kunyuan <kunyuany@163.com>
2023-05-03 15:41:51 +0800

    Update test

--- tests/test_chat_save.py
+++ tests/test_chat_save.py
@@ -13,24 +13,24 @@ class TestChatSaver:
     @classmethod
     def teardown_class(cls):
         rm("Log/*.log")
-        rm("chat*.txt")
+        rm("Log/chat*.txt")
 
     def test_init(self, saver: ChatSaver):
-        assert saver.chat_file == "chat_0.txt"
+        assert saver.chat_file.endswith("chat_0.txt")
 
     def test_add_chat(self, saver: ChatSaver):
         saver.add_chat({"id": 1, "content": "hello"})
-        assert saver.chat_file == "chat_0.txt"
+        assert saver.chat_file.endswith("chat_0.txt")
         saver.add_chat({"id": 2, "content": "hello"})
-        assert saver.chat_file == "chat_0.txt"
+        assert saver.chat_file.endswith("chat_0.txt")
         saver.add_chat({"id": 3, "content": "hello"})
-        assert saver.chat_file == "chat_1.txt"
+        assert saver.chat_file.endswith("chat_1.txt")
         saver.add_chat({"id": 4, "content": "hello"})
-        assert saver.chat_file == "chat_1.txt"
+        assert saver.chat_file.endswith("chat_1.txt")
         saver.add_chat({"id": 5, "content": "hello"})
-        assert saver.chat_file == "chat_2.txt"
+        assert saver.chat_file.endswith("chat_2.txt")
 
     def test_init_file(self):
         saver = ChatSaver()
-        assert saver.chat_file == "chat_3.txt"
+        assert saver.chat_file.endswith("chat_3.txt")
 
