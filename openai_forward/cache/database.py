0d697af5725beea9e3970b0c071ee6574a98de49
K.Y <beidongjiedeguang@gmail.com>
2023-12-26 21:12:24 +0800

    Upgrade flaxkv

--- openai_forward/cache/database.py
+++ openai_forward/cache/database.py
@@ -2,74 +2,29 @@ from flaxkv import FlaxKV
 
 from ..settings import CACHE_BACKEND, CACHE_ROOT_PATH_OR_URL, LOG_CACHE_DB_INFO
 
-cache = True
-
-if CACHE_BACKEND.upper() == "LMDB":
-    try:
-        import lmdb
-    except ImportError:
-        raise ImportError("Please install LMDB: pip install lmdb")
-
-    if LOG_CACHE_DB_INFO:
-        db_dict = FlaxKV(
-            "CACHE_LMDB",
-            root_path_or_url=CACHE_ROOT_PATH_OR_URL,
-            backend='lmdb',
-            cache=cache,
-            log="INFO",
-            save_log=True,
-        )
-    else:
-        db_dict = FlaxKV(
-            "CACHE_LMDB",
-            root_path_or_url=CACHE_ROOT_PATH_OR_URL,
-            backend='lmdb',
-            cache=cache,
-        )
-
-elif CACHE_BACKEND.upper() == "LEVELDB":
-    try:
-        import plyvel
-    except ImportError:
-        raise ImportError("Please install LevelDB: pip install plyvel")
-
-    if LOG_CACHE_DB_INFO:
-        db_dict = FlaxKV(
-            "CACHE_LEVELDB",
-            root_path_or_url=CACHE_ROOT_PATH_OR_URL,
-            backend='leveldb',
-            cache=cache,
-            log="INFO",
-            save_log=True,
-        )
-    else:
-        db_dict = FlaxKV(
-            "CACHE_LEVELDB",
-            root_path_or_url=CACHE_ROOT_PATH_OR_URL,
-            backend='leveldb',
-            cache=cache,
-        )
-
-elif CACHE_BACKEND.upper() == "REMOTE":
-    if LOG_CACHE_DB_INFO:
-        db_dict = FlaxKV(
-            "REMOTE_DB",
-            root_path_or_url=CACHE_ROOT_PATH_OR_URL,
-            backend="leveldb",
-            cache=cache,
-            log="INFO",
-            save_log=True,
-        )
-    else:
-        db_dict = FlaxKV(
-            "REMOTE_DB",
-            root_path_or_url=CACHE_ROOT_PATH_OR_URL,
-            backend="leveldb",
-            cache=cache,
-        )
-
-elif CACHE_BACKEND.upper() == "MEMORY":
+if CACHE_BACKEND.upper() == "MEMORY":
     db_dict = {}
 
+elif CACHE_BACKEND.lower() in ("leveldb", "lmdb"):
+
+    if LOG_CACHE_DB_INFO:
+        db_dict = FlaxKV(
+            "CACHE_DB",
+            root_path_or_url=CACHE_ROOT_PATH_OR_URL,
+            backend=CACHE_BACKEND.lower(),
+            cache=True,
+            log="INFO",
+            save_log=True,
+        )
+    else:
+        db_dict = FlaxKV(
+            "CACHE_DB",
+            root_path_or_url=CACHE_ROOT_PATH_OR_URL,
+            backend=CACHE_BACKEND.lower(),
+            cache=True,
+        )
+
 else:
-    raise ValueError(f"Unknown cache backend: {CACHE_BACKEND}")
+    raise ValueError(
+        f"Unknown cache backend: {CACHE_BACKEND}. The valid backends are: 'leveldb', 'lmdb', 'memory'"
+    )
