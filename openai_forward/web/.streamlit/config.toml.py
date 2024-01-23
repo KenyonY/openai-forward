9f10e778a6f11ff917aed80f02dc856dd0224e03
K.Y <beidongjiedeguang@gmail.com>
2024-01-24 02:11:59 +0800

    fix config.toml

--- openai_forward/web/.streamlit/config.toml.py
+++
@@ -1,8 +0,0 @@
-# this is needed for local development with docker
-[server]
-# if you don't want to start the default browser:
-headless = true
-# you will need this for local development:
-runOnSave = true
-# you will need this if running docker on windows host:
-fileWatcherType = "poll"
