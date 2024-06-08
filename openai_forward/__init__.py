b0f1cedbde262e2453332d636cef6a26706bbd28
K.Y <beidongjiedeguang@gmail.com>
2024-06-08 18:03:48 +0800

    feat: Change the env variable `OPENAI_API_KEY` to `OPENAI_API_KEY_CONFIG`

--- openai_forward/__init__.py
+++ openai_forward/__init__.py
@@ -1,20 +1,6 @@
-__version__ = "0.8.0"
+__version__ = "0.8.1"
 
 from dotenv import load_dotenv
 from yaml import load
 
-
-def yaml_load(filepath):
-
-    try:
-        from yaml import CLoader as Loader
-    except ImportError:
-        from yaml import Loader
-    with open(filepath, mode='r', encoding="utf-8") as stream:
-        #     stream = stream.read()
-        content = load(stream, Loader=Loader)
-    return content
-
-
-# yaml_load()
 load_dotenv('.env', override=False)
--- openai_forward/settings.py
+++ openai_forward/settings.py
@@ -119,8 +119,8 @@ if CACHE_GENERAL:
 IP_WHITELIST = env2list("IP_WHITELIST", sep=ENV_VAR_SEP)
 IP_BLACKLIST = env2list("IP_BLACKLIST", sep=ENV_VAR_SEP)
 
-OPENAI_API_KEY = env2dict("OPENAI_API_KEY")
-FWD_KEY = env2dict("FORWARD_KEY")
+OPENAI_API_KEY = env2dict("OPENAI_API_KEY_CONFIG")
+FWD_KEY = env2dict("FORWARD_KEY_CONFIG")
 LEVEL_MODELS = {int(key): value for key, value in env2dict("LEVEL_MODELS").items()}
 
 PROXY = os.environ.get("PROXY", "").strip() or None
